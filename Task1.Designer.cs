3f0e0fda5caeda67e2b445e8bc51736f9067c7f5
Машина Паша <evtuh.pania@yandex.ru>
2021-10-06 01:08:52 +0800

    Написал первое задание

--- Task1.Designer.cs
+++ Task1.Designer.cs
@@ -30,33 +30,120 @@ namespace GUI
         private void InitializeComponent()
         {
             this.back = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountCost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOfTask1 = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // back
             // 
-            this.back.Location = new System.Drawing.Point(12, 263);
+            this.back.Location = new System.Drawing.Point(12, 107);
             this.back.Name = "back";
-            this.back.Size = new System.Drawing.Size(101, 43);
+            this.back.Size = new System.Drawing.Size(114, 51);
             this.back.TabIndex = 0;
             this.back.Text = "Назад";
             this.back.UseVisualStyleBackColor = true;
             this.back.Click += new System.EventHandler(this.back_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(638, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Дано натуральное число 1 ≤ n ≤ 9999, определяющее стоимость товара в копейках. \r\n" +
+    "Выразить стоимость в рублях и копейках, например, 3 рубля 21 копейка, 15 рублей " +
+    "5 копеек, 1 рубль ровно и т. п.";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // amountCost
+            // 
+            this.amountCost.Location = new System.Drawing.Point(360, 52);
+            this.amountCost.Name = "amountCost";
+            this.amountCost.Size = new System.Drawing.Size(146, 23);
+            this.amountCost.TabIndex = 2;
+            this.amountCost.TextChanged += new System.EventHandler(this.amountCost_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Стоимость товара в копейках:";
+            // 
+            // buttonOfTask1
+            // 
+            this.buttonOfTask1.Location = new System.Drawing.Point(276, 121);
+            this.buttonOfTask1.Name = "buttonOfTask1";
+            this.buttonOfTask1.Size = new System.Drawing.Size(114, 37);
+            this.buttonOfTask1.TabIndex = 4;
+            this.buttonOfTask1.Text = "Выполнить";
+            this.buttonOfTask1.UseVisualStyleBackColor = true;
+            this.buttonOfTask1.Click += new System.EventHandler(this.buttonOfTask1_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(534, 107);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(114, 51);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Очистить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(360, 81);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(146, 23);
+            this.answerBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ответ:";
+            // 
             // Task1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 318);
+            this.ClientSize = new System.Drawing.Size(660, 170);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonOfTask1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.amountCost);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.back);
             this.Name = "Task1";
             this.Text = "Task1";
             this.Load += new System.EventHandler(this.Task1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountCost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOfTask1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Task1.cs
+++ Task1.cs
@@ -17,6 +17,8 @@ namespace GUI
         public Task1()
         {
             InitializeComponent();
+            amountCost.Text = Properties.Settings.Default.cost.ToString();
+            answerBox.Text = Properties.Settings.Default.answer;
         }
 
         private void back_Click(object sender, EventArgs e)
@@ -34,6 +36,110 @@ namespace GUI
         private void Task1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOfTask1_Click(object sender, EventArgs e)
+        {
+            int cost;
+
+            try
+            {
+                cost = int.Parse(this.amountCost.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(cost > 9999)
+            {
+                MessageBox.Show("Введенно число больше 9999!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Properties.Settings.Default.cost = cost;
+
+            var resultRubKop = Logic.RubKop(cost);
+            var resultWords = Logic.WordsRubKop(resultRubKop.rub, resultRubKop.kop);
+
+            string answer = resultRubKop.rub.ToString() + " " + resultWords.rubstr + " " + resultRubKop.kop.ToString() + " " + resultWords.kopstr;
+
+            answerBox.Text = answer;
+
+            Properties.Settings.Default.answer = answer;
+            Properties.Settings.Default.Save();
+        }
+
+        private void amountCost_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            amountCost.Text = "";
+            answerBox.Text = "";
+        }
+    }
+    public class ResultRubKop
+    {
+        public int rub;
+        public int kop;
+    }
+
+    //Класс окончаний
+    public class ResultWords
+    {
+        public string rubstr = "Рублей";
+        public string kopstr = "Копеек";
+    }
+
+    public class Logic
+    {
+        public static ResultRubKop RubKop(int cost)
+        {
+            int _rub = cost / 100;
+            int _kop = cost % 100;
+            return new ResultRubKop
+            {
+                rub = _rub,
+                kop = _kop
+            };
+        }
+        public static ResultWords WordsRubKop(int rub, int kop)
+        {
+            string _rubstr = "Рублей";
+            string _kopstr = "Копеек";
+            if ((rub % 10 == 1) && (rub != 11))  //Определение окончания "Рубль"                                                                             
+            {
+                _rubstr = "Рубль";
+            }
+
+            if ((kop % 10 == 1) && (kop != 11)) //Определение окончания "Копейка"
+            {
+                _kopstr = "Копейка";
+            }
+
+            //Определение окончания "Рубля"
+            if (((rub % 10 == 2) && (rub != 12)) || ((rub % 10 == 3) && (rub != 13)) || ((rub % 10 == 4) && (rub != 14)))
+            {
+                _rubstr = "Рубля";
+            }
+            //Определение окончания "Копейки"
+            if (((kop % 10 == 2) && (kop != 12)) || ((kop % 10 == 3) && (kop != 13)) || ((kop % 10 == 4) && (kop != 14)))
+            {
+                _kopstr = "Копейки";
+            }
+            return new ResultWords
+            {
+                rubstr = _rubstr,
+                kopstr = _kopstr
+            };
         }
     }
 }
